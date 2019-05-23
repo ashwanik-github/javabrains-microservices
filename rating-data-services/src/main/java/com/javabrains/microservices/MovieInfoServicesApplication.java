a41119b270c995e7716667464b4c41a74fb3c176
ashwaniK <ashwaniK@AshwaniK-VM>
2019-05-23 20:55:40 +0530

    first commit

---
+++ rating-data-services/src/main/java/com/javabrains/microservices/MovieInfoServicesApplication.java
@@ -0,0 +1,13 @@
+package com.javabrains.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieInfoServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieInfoServicesApplication.class, args);
+	}
+
+}
